9acc1c80aaccb8829ba1000b87b0f35e1990569f
evo-vii <sanicolmg@gmail.com>
2026-03-04 11:24:52 +0800

    first commit

---
+++ [T9 - S15] MP_MAIN.c
@@ -0,0 +1,105 @@
+/******************************************************************************
+ *  Description     : This program is an interpretation of the logical messages from
+                      the specifications of the CCDSTRU Machine Project.
+ *  Author/s        : Borlasa, Sebastian O.
+                      Sanico, Lance Matthew G.
+ *                    Toledo, Nyle M.
+ *  Section         : S15
+ *  Last Modified   : 03/04/2026 @ 11:15am
+ *  Acknowledgments : <list of references used in the making of this project>
+ ******************************************************************************/
+
+// PRE-PROCESSOR DIRECTIVES------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+
+
+
+// DEFINITIONS-------------------------------------------------------------------------------------------------------------------------------
+
+// FUNCTIONS---------------------------------------------------------------------------------------------------------------------------------
+
+//    (DEVNOTE - Lance: All functions are defaulted to void, please assume they are not yet fully interpreted - 03/04/26)
+
+/*
+  <function description + params>
+*/
+void Remove()
+{
+
+
+
+}
+
+/*
+  <function description + params>
+*/
+void Replace()
+{
+
+
+
+}
+
+/*
+  <function description + params>
+*/
+void Expand()
+{
+
+
+
+}
+
+/*
+  <function description + params>
+*/
+void Update()
+{
+
+
+
+}
+
+/*
+  <function description + params>
+*/
+void NextPlayerMove()
+{
+
+
+
+}
+
+/*
+  <function description + params>
+*/
+void GameOver()
+{
+
+
+
+}
+
+
+// MAIN--------------------------------------------------------------------------------------------------------------------------------------
+
+int main()
+{
+
+
+  return 0;
+}
+
+/**
+ * This is to certify that this project is our own work, based on our personal
+ * efforts in studying and applying the concepts learned. We have constructed the
+ * functions and their respective algorithms and corresponding code by ourselves.
+ * The program was run, tested, and debugged by our own efforts. We further certify
+ * that we have not copied in part or whole or otherwise plagiarized the work of other
+ * students and/or persons, nor did we employ the use of AI in any part of the deliverable.
+ 
+   Borlasa, Sebastian O.
+   Sanico, Lance Matthew G.
+   Toledo, Nyle M.
+
+ */
